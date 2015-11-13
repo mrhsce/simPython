46a996a100e8f2e4616245c4a8565f3cf38038e1
mehdi <mehdi.bagvand@gmail.com>
2015-11-13 15:57:01 +0330

    create component
    create component

---
+++ Create.py
@@ -0,0 +1,50 @@
+from Entity import Entity
+from abc import ABCMeta, abstractmethod
+
+
+class Create(Entity):
+
+    def __init__(self, entity_type, name, create_type):
+        super(Create, self).__init__(entity_type)
+        self.name = name
+        self.create_type = create_type
+
+    def body(self):
+        self.create_type.start()
+
+
+class CreateType(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, time_unit, first_creation, entity_per_arrival, max_arrival):
+        self.time_unit = time_unit
+        self.first_creation = first_creation
+        self.entity_per_arrival = entity_per_arrival
+        self.max_arrival = max_arrival
+
+    @abstractmethod
+    def start(self):
+        print "Error: your child class must have start method!!"
+
+
+class ConstantType(CreateType):
+
+    def __init__(self, time_unit, first_creation, entity_per_arrival, max_arrival):
+        super(ConstantType, self).__init__(time_unit, first_creation, entity_per_arrival, max_arrival)
+
+    def start(self):
+        pass
+
+
+class RandomType(CreateType):
+
+    def __init__(self, time_unit, first_creation, entity_per_arrival, max_arrival):
+        super(RandomType, self).__init__(time_unit, first_creation, entity_per_arrival, max_arrival)
+
+    def start(self):
+        pass
+
+
+r = RandomType("h", 0, 1, -1)
+c = Create("Entity1", "create1",r)
+
