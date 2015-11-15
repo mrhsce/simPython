929ff7ce7915f4cb6ad3f69de8308eb80c7d55fc
mehdi <mehdi.bagvand@gmail.com>
2015-11-15 22:56:29 +0330

    singleTon
    singleTon

---
+++ Event.py
@@ -0,0 +1,38 @@
+from Singleton import *
+import heapq
+
+
+class Event(object):
+
+    def __init__(self, entityPointer, funcName, params, execTime):
+        self.entityPointer = entityPointer
+        self.funcName = funcName
+        self.params = params
+        self.execTime = execTime
+
+
+@Singleton
+class EventList:
+    def __init__(self):
+        print 'Foo created'
+
+
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
--- Process.py
+++ Process.py
@@ -7,6 +7,7 @@ class Process(Entity):
     def __init__(self, Type, id, inputPointer, outputPointer, name, customerDelayStatDis):
         super(Process, self).__init__(Type, id, inputPointer, outputPointer)
         self.name = name
+        self.queue = 0
         if customerDelayStatDis == 0:
             self.customerDelayStatDis = UniformDis(1, 10)
         else:
