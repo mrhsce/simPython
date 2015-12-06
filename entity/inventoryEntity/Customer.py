ab4ba111ef9c437472101e198e8b0b616541cad0
MrHsCE <mrhsce@gmail.com>
2015-12-06 23:53:45 +0330

    final

--- entity/inventoryEntity/Customer.py
+++ entity/inventoryEntity/Customer.py
@@ -4,6 +4,7 @@ from InventoryEntity import InventoryEntity
 class Customer(InventoryEntity):
     def __init__(self, simSystem, Type, id, inputPointer, outputPointer):
         super(Customer, self).__init__(simSystem, Type, id, inputPointer, outputPointer)
+        self.orderStatDist = 0
 
     def takeOrder(self, amount):
         pass
@@ -15,6 +16,6 @@ class Customer(InventoryEntity):
         self.outputPointer.append(other)
         other.inputPointer.append(self)
 
-    def setStatDis(self, createStatDis):
-        self.createStatDis = createStatDis
+    def setStatDis(self, orderStatDist):
+        self.orderStatDist = orderStatDist
 
